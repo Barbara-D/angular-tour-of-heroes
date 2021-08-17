45a4ed8031cf8543a010bb7d74d6b6be8d83b564
Barbara-D <bdeskar@gmail.com>
2021-08-17 10:35:29 +0200

    Create app and display a list of characters

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Ren Amamya', codename:'Joker'},
+    {id: 12, name: 'Ryuji Sakamoto', codename: 'Skull'},
+    {id: 13, name: 'Morgana', codename: 'Mona'},
+    {id: 14, name: 'Ann Takamaki', codename: 'Panther'},
+    {id: 15, name: 'Yusuke Kitagawa', codename: 'Fox'},
+    {id: 16, name: 'Makoto Niijima', codename: 'Queen'},
+    {id: 17, name: 'Futaba Sakura', codename: 'Oracle'},
+    {id: 18, name: 'Haru Okumura', codename: 'Noir'},
+    {id: 19, name: 'Goro Akechi', codename: 'Crow'},
+
+
+
+];
